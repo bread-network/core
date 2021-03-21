cb35e6f76192103881ede6630db538a1c0b38edf
Chirag Jain <jainchg@gmail.com>
2021-03-21 23:53:00 +0530

    add router URLs

--- breadEngine/api/urls.py
+++ breadEngine/api/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from breadEngine.api.viewsets import PostViewSet, UserViewSet, CurrentUserViewSet
+
 router = DefaultRouter()
-# TODO: Register Routers
+router.register('posts', PostViewSet, basename='posts')
+router.register('users', UserViewSet, basename='users')
+router.register('me', CurrentUserViewSet, basename='me')
 
 urlpatterns = router.urls
